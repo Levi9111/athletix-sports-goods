4e5efd3f8555cf66bfce206fc2fef193f43c0721
Levi9111 <captainlevi9111@gmail.com>
2024-08-25 15:51:50 +0600

    first commit, basic setup complete

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div>
+      <Button variant='default' size='default'>Add me</Button>
+    </div>
+  )
+}
+
+export default App
